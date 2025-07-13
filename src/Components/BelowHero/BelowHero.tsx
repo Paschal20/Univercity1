9440c83ebfe3623e1278e325565ef522b904b562
Paschal Anaedu <170808501@live.unilag.edu.ng>
2025-07-13 07:08:47 +0100

    second Commit

--- src/Components/BelowHero/BelowHero.tsx
+++ src/Components/BelowHero/BelowHero.tsx
@@ -1,78 +1,191 @@
-import React from "react";
+// import React from "react";
 import styled from "@emotion/styled";
+import BH1 from "../../assets/Images/Bh 1.jpeg";
+import BH2 from "../../assets/Images/Bh 2.jpeg";
+import BH3 from "../../assets/Images/Bh 3.png";
 
+const Section = styled.section`
+  min-height: 100vh;
+  height: 160vh;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-
   align-items: center;
-  height: 160vh;
-  background-color: #f5f5f5;
-  color: #333;
-  font-size: 24px;
-  font-weight: bold;
-  text-align: center;
-  padding: 20px;
-`;
-const CardHolder = styled.div`
-  width: 90%;
-  background-color: blue;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
+  width: 86%;
+  max-width: 1200px;
 `;
 
-const Card = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-
-  width: 48%;
-  background-color: black;
-  border-radius: 10px;
-  padding: 20px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+const Title = styled.h1`
+  font-size: 35px;
+  font-weight: 700;
+  color: rgb(23, 53, 92);
+  margin-bottom: 5px;
 `;
-const CardTitle = styled.h2`
-  font-size: 24px;
-  font-weight: bold;
+
+const Content = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+  color: rgb(46, 66, 92)
   margin-bottom: 10px;
 `;
+const CardHolder = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 1200px;
+  margin-top: 30px;
+  margin-bottom: 50px;
+`;
+const Card = styled.div`
+  width: 360px;
 
-const CardText = styled.p`
+  border-radius: 10px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  margin: 20px;
+  color: white;
+`;
+
+const ImageHolder = styled.div`
+  width: 100%;
+  height: 200px;
+  border-radius: 10px;
+`;
+const TextHolder = styled.div`
+  width: 350px;
+  margin-left: 20px;'
+  
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const CardTitle = styled.h3`
+  font-size: 22px;
+  font-weight: 700;
+  margin-bottom: 10px;
+  color: rgb(23, 53, 92);
+  margin-top: 30px;
+`;
+
+const CardContent = styled.p`
   font-size: 16px;
   font-weight: 400;
   margin-bottom: 20px;
   color: rgb(46, 66, 92);
-  text-align: left;
+`;
+
+const BottomCardWords = styled.div`
+  margin-left: 20px;
+  margin-top: 10px;
+  width: 240px;
+  display: flex;
+  justify-content: space-between;
+`;
+const BottomWord = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+  margin-bottom: 20px;
+  color: rgb(46, 66, 92);
 `;
 const CardButton = styled.button`
   background-color: #60c26a;
   box-shadow: 0px 5px 4px rgba(0, 0, 0, 0.1);
   color: white;
-  padding: 13px 23px;
+  padding: 15px 25px;
   border: none;
   border-radius: 3px;
   cursor: pointer;
-  margin-left: 10px;
-  transition: background-color 0.3s ease;
 `;
 
 const BelowHero = () => {
   return (
-    <Container>
-      <CardHolder>
-        <Card>
-          <CardTitle>Individuals</CardTitle>
-          <CardText>
-            Accelerate your dream of becoming a world-class software developer
-            or designer.
-          </CardText>
-          <Card></Card>
-        </Card>
-        <Card></Card>
-      </CardHolder>
-    </Container>
+    <>
+      <Section>
+        <Container>
+          <Title>Find your Perfect program</Title>
+          <Content>
+            Our curriculum uses a project-based learning approach and is perfect
+            for both beginners and experienced individuals.
+          </Content>
+
+          <CardHolder>
+            <Card>
+              <ImageHolder>
+                <Image src={BH1} alt="Image1" />
+              </ImageHolder>
+
+              <TextHolder>
+                <CardTitle>Machine Learning</CardTitle>
+                <CardContent>
+                  Welcome to the Machine Learning Fundamentals course! In this
+                  12-week curriculum, you will embark on an exciting journey to
+                  explore the world of machine…
+                </CardContent>
+                <BottomCardWords>
+                  <BottomWord>12 months</BottomWord>
+                  <BottomWord>Twice a week</BottomWord>
+                </BottomCardWords>
+              </TextHolder>
+            </Card>
+
+            <Card>
+              <ImageHolder>
+                <Image src={BH2} alt="Image1" />
+              </ImageHolder>
+
+              <TextHolder>
+                <CardTitle>Cybersecurity (Ethical Hacking)</CardTitle>
+                <CardContent>
+                  Cybersecurity has an estimated 37% growth rate for jobs,
+                  faster than the average for other occupations. Gain skills in
+                  the occupation with the fastest…
+                </CardContent>
+
+                <BottomCardWords>
+                  <BottomWord>12 months</BottomWord>
+                  <BottomWord>3 months</BottomWord>
+                </BottomCardWords>
+              </TextHolder>
+            </Card>
+
+            <Card>
+              <ImageHolder>
+                <Image src={BH3} alt="Image1" />
+              </ImageHolder>
+
+              <TextHolder>
+                <CardTitle>FullStack Web Development</CardTitle>
+                <CardContent>
+                  Are you ready to embark on an exciting journey into the world
+                  of full-stack development? Our comprehensive Full-Stack
+                  Development Course is your gateway to…
+                </CardContent>
+                <BottomCardWords>
+                  <BottomWord>6 months</BottomWord>
+                  <BottomWord>Four times a week</BottomWord>
+                </BottomCardWords>
+              </TextHolder>
+            </Card>
+          </CardHolder>
+          <CardButton>Browse all Courses</CardButton>
+        </Container>
+      </Section>
+    </>
   );
 };
 
--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -22,17 +22,59 @@ const Hero2 = styled.div`
   background: rgba(0, 0, 0, 0.65);
   color: white;
   display: flex;
-  
   flex-direction: column;
+  align-items: center;
   text-align: center;
   font-size: 45px;
   font-weight: 700;
-  &>span {
+  & > span {
     margin-top: 150px;
   }
-
 `;
 
+const CardHolder = styled.div`
+  width: 90%;
+  margin-top: 120px;
+  z index: 100;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  background-color: white;
+  width: 48%;
+
+  border-radius: 10px;
+  padding: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+`;
+const CardTitle = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 10px;
+  color: rgb(23, 53, 92);
+`;
+
+const CardText = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+  margin-bottom: 30px;
+  color: rgb(46, 66, 92);
+  text-align: left;
+`;
+const CardButton = styled.button`
+  background-color: #60c26a;
+  box-shadow: 0px 5px 4px rgba(0, 0, 0, 0.1);
+  color: white;
+  padding: 15px 25px;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+`;
 
 const Hero: React.FC = () => {
   return (
@@ -42,6 +84,26 @@ const Hero: React.FC = () => {
           <span>
             Accelerate Your Carrer <br /> In Tech
           </span>
+
+          <CardHolder>
+            <Card>
+              <CardTitle>Individuals</CardTitle>
+              <CardText>
+                Accelerate your dream of becoming a world-class software
+                developer or designer.
+              </CardText>
+              <CardButton>Browse Courses</CardButton>
+            </Card>
+
+            <Card>
+              <CardTitle>Corporate</CardTitle>
+              <CardText>
+                Professional Staff training courses applicable to any corporate
+                organization.
+              </CardText>
+              <CardButton>Learn More</CardButton>
+            </Card>
+          </CardHolder>
         </Hero2>
       </Hero1>
     </>
